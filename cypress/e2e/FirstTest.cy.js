daf5b0d318ef4244ed1a1afce4dd98cff2222c1e
Ad4msvensson <adampereric@icloud.com>
2024-11-08 09:14:13 +0100

    psuha

--- cypress/e2e/FirstTest.cy.js
+++ cypress/e2e/FirstTest.cy.js
@@ -1,6 +1,20 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('http://127.0.0.1:5500/public/html/index.html')
-  })
-})
+// Import cypress-axe
+import 'cypress-axe';
 
+describe('Accessibility Testing', () => {
+  beforeEach(() => {
+    // Visit the page you want to test
+    cy.visit('your-page-url');
+    // Inject the Axe-core library into the page
+    cy.injectAxe();
+  });
+
+  it('should have no detectable accessibility violations on load', () => {
+    // Run the accessibility checks using Axe-core
+    cy.checkA11y(null, null, (violations) => {
+      if (violations.length) {
+        throw new Error(`Accessibility issues found: ${violations.length}`);
+      }
+    });
+  });
+});
